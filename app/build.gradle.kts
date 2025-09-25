7cf6d778edc84e2b4d131d6470983c801ce25b21
yamidev52 <sergioyamilng2005@gmail.com>
2025-09-24 23:46:07 -0600

    ANIMACION Y PROYECTO

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.yamidev.actividad6"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.yamidev.actividad6"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
